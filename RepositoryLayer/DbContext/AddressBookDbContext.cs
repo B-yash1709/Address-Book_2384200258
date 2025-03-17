75805b50787bc793b6fede7e16efb5c1ce574717
B-yash1709 <bhardwajyash4u@gmail.com>
2025-03-17 11:41:14 +0530

    [Yashovardhan Bhardwaj] Added: Code added to branch

---
+++ RepositoryLayer/DbContext/AddressBookDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RepositoryLayer.AddressBookEntity;
+
+namespace RepositoryLayer.Context;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<AddressBookEntity.AddressBookEntity> Contacts { get; set; } // Define your entities
+}
